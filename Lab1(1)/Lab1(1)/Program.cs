65e89107ca720e1b7bf0e094d2860f95b16d0872
Taras Datskiv <64806508+tarasdatskiv@users.noreply.github.com>
2023-11-17 16:22:48 +0200

    Add files via upload

---
+++ Lab1(1)/Lab1(1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab1_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5;
+            double b = 5;
+            double x = 5;
+
+            double sign = Math.Sign(a*x+b);
+            double abs = Math.Abs(x-b);
+            double y = Math.Sqrt(sign - abs);
+
+            Console.WriteLine(y);
+
+        }
+    }
+}
